a0a3658aa846f59082bcff1ee0da05ca59642b6e
akotetsu <yuanliqu@gmail.com>
2025-02-17 21:39:20 +0900

    Initial Commit

---
+++ NoteApp/NoteAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteAppApp.swift
+//  NoteApp
+//
+//  Created by 原里駆 on 2025/02/17.
+//
+
+import SwiftUI
+
+@main
+struct NoteAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
