a762eba46ed9b942400b6dd892840ede50fa6495
akotetsu <yuanliqu@gmail.com>
2025-02-19 16:05:01 +0900

    first commit

---
+++ NoteApp/NoteEditView.swift
@@ -0,0 +1,8 @@
+//
+//  NoteEditView.swift
+//  NoteApp
+//
+//  Created by 原里駆 on 2025/02/18.
+//
+
+import Foundation
---
+++ NoteApp/NoteTextView.swift
@@ -0,0 +1,8 @@
+//
+//  NoteTextView.swift
+//  NoteApp
+//
+//  Created by 原里駆 on 2025/02/17.
+//
+
+import Foundation
